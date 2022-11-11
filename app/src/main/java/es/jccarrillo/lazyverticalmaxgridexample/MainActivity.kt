37aabbbd0eba77d05334ccc2e3e4e324c104cffd
Juan Carlos Carrillo <juan.carrillo@mango.com>
2022-11-11 21:36:07 +0100

    Modified MainActivity to use Viewmodel and component Screen

--- app/src/main/java/es/jccarrillo/lazyverticalmaxgridexample/MainActivity.kt
+++ app/src/main/java/es/jccarrillo/lazyverticalmaxgridexample/MainActivity.kt
@@ -3,41 +3,46 @@ package es.jccarrillo.lazyverticalmaxgridexample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.activity.viewModels
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import es.jccarrillo.lazyverticalmaxgrid.LazyVerticalMaxGrid
+import es.jccarrillo.lazyverticalmaxgridexample.ui.component.MainItem
+import es.jccarrillo.lazyverticalmaxgridexample.ui.component.MainList
+import es.jccarrillo.lazyverticalmaxgridexample.ui.component.MainScreen
 import es.jccarrillo.lazyverticalmaxgridexample.ui.theme.LazyVerticalMaxGridExampleTheme
+import es.jccarrillo.lazyverticalmaxgridexample.ui.viewmodel.MainViewModel
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             LazyVerticalMaxGridExampleTheme {
-                // A surface container using the 'background' color from the theme
+
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier
+                        .fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    MainScreen(viewModel)
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    LazyVerticalMaxGridExampleTheme {
-        Greeting("Android")
-    }
-}
